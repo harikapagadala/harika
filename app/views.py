94f0a0f3e8a4a07bc42c3ffc52a4b090d66576d0
Your Name <you@example.com>
2022-11-22 14:53:34 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+from django.shortcuts import render
+from django.http import HttpResopens, HttpResponse
+# Create your views here.
+def frist(reuest):
+    return HttpResponse ('AMMA & NANNA I LOVE YOU SO MACH AND FOREVER')
